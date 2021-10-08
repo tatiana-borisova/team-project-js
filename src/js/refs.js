d88a3d5e46c9192f7576261f1f3d8f2c8fa68c98
Tatiana Borisova <81636916+tatiana-borisova@users.noreply.github.com>
2021-10-08 21:23:45 +0200

    switches links home and library + styles

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,1 +1,6 @@
-export default {};
+export default {
+  homeLink: document.querySelector('.header-links__item--home'),
+  libraryLink: document.querySelector('.header-links__item--library'),
+  libraryBtns: document.querySelector('.header-library__list'),
+  searchForm: document.querySelector('.header-form'),
+};
