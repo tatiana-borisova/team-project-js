e796e8591aa605982523c3881afa8cf9a265c63f
tatiana-borisova <81636916+tatiana-borisova@users.noreply.github.com>
2021-10-09 11:37:43 +0000

    Deploying to gh-pages from @ tatiana-borisova/team-project-js@ea32ca7361416632f7e83a54288fb58df3aeefd1 🚀

--- src.06338ba0.js
+++ src.06338ba0.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"../refs":"VyiV"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/header/header");
 },{"./sass/main.scss":"clu1","./js/header/header":"N0LX"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-js/src.b0bd8368.js.map
+//# sourceMappingURL=/team-project-js/src.06338ba0.js.map
