dd56d64e25881030492d51a75fa99825ff494f7d
Yana Nechveeva <79226190+Yana1112pstr@users.noreply.github.com>
2021-10-10 17:09:46 +0300

    update header
    Стилизировала инпут и поставила бекграунд на хедер, который переключается на кнопках "home", "my library"

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -7,9 +7,14 @@ function onHomeEl(e) {
   e.preventDefault();
   refs.libraryBtns.classList.add('is-hidden');
   refs.searchForm.classList.remove('is-hidden');
+  refs.headerBackground.classList.remove('header-background__library');
+  refs.headerBackground.classList.add('header-background');
+  
 }
 function onLibraryEl(e) {
   e.preventDefault();
   refs.libraryBtns.classList.remove('is-hidden');
   refs.searchForm.classList.add('is-hidden');
+  refs.headerBackground.classList.remove('header-background');
+  refs.headerBackground.classList.add('header-background__library');
 }
