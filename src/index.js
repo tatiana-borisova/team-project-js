5e8ead56e76804d7e17fd7a8007c626d14824a60
Toni1993 <77364364+Toni1993@users.noreply.github.com>
2021-10-11 22:51:35 +0200

    check-theme

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+
 import './sass/main.scss';
 import './js/header/header';
+import './js/check-theme'
+
--- src/js/consts.js
+++ src/js/consts.js
@@ -1,4 +1,29 @@
 const API_KEY = 'f205ff9551fdba7fe2c499379d3dfbc2';
 const URL = 'https://api.themoviedb.org';
 
-export { API_KEY, URL };
+// export { API_KEY, URL };
+
+export async function getPopularFilms() {
+   try {
+      const response = await fetch(`${URL}/3/trending/all/day?api_key=${API_KEY}`);
+      return await response.json();
+   }
+   catch (error) {
+      console.error(error);
+   }
+}
+
+export async function getGenreList() {
+  try {
+      const response = await fetch(`${URL}/3/genre/movie/list?api_key=${API_KEY}`);
+      const data = await response.json()
+      return data.genres
+   }
+   catch (error) {
+      console.error(error);
+  }
+}
+
+// https://api.themoviedb.org/3/movie/505?api_key=f205ff9551fdba7fe2c499379d3dfbc2
+
+// https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
